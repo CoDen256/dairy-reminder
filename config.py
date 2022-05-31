6b700bd7c87aea066c695ca510628aac5e7e63ab
Denys Chernyshov <den.blackshov@gmail.com>
2022-05-31 14:21:19 +0200

    IMplement inserting for last time

--- config.py
+++ config.py
@@ -3,7 +3,7 @@ from util import read_token
 
 log.basicConfig(level=log.INFO)
 id = 283382228
-time = "11:58"
+time = "14:22"
 token = read_token("token")
 notion_token = read_token("notion_token")
 notion_database_id = "c14f4828-2090-4e5b-aa64-e290a18a181d"
--- reminder.py
+++ reminder.py
@@ -9,38 +9,38 @@ class Reminder:
         self.notify = notify
 
     def check_month(self, db):
-        (last_date_str, last_text) = db.get_last_reminder()
+        (last_date_str, _) = db.get_last_reminder()
         current_date = datetime.today()
         last_date = datetime.strptime(last_date_str, "%Y/%m")
-        log.info(f"Checking: {current_date} > {last_date} + 62 days ?",)
-        return (current_date - last_date).days >= 62
+        log.info(f"Checking: {current_date} > {last_date} + 62 days ?")
+        return ((current_date - last_date).days >= 62, last_date)
 
     def check_and_send_reminder(self):
         log.info("Checking reminder...")
         with ReminderDatabase(self.config) as db:
-            if(self.check_month(db)):
+            (too_old, last_month) = self.check_month(db)
+            if too_old:
+                next_month = self.get_next_month(last_month)
                 log.info("Check successful, sending the reminder...")
-                self.notify("It's time to send an entry to your diary:")
+                self.notify(
+                    f"It's time to send an entry to your diary: {next_month}")
             else:
                 log.info("Check failed, skipping...")
 
     def process_entry(self, entry):
-        new = False
         log.info(f"Processing entry: {entry}")
         with ReminderDatabase(self.config) as db:
-            new = self.check_month(db)
-            if new:
-                date = self.get_previous_month()
-                log.info(f"Check successful, inserting for previous month: {date}")
-                
-                self.publish(db, date, entry)
-            return (new, " -> ".join(list(db.get_last_reminder())))
+            (too_old, last_month) = self.check_month(db)
+            if too_old:
+                next_month = self.get_next_month(last_month)
+                log.info(
+                    f"Check successful, inserting for next not filled month: {next_month}")
 
-    def get_previous_month(self):
-        today = datetime.today()
-        first = today.replace(day=1)
-        lastMonth = first - timedelta(days=1)
-        return lastMonth.strftime("%Y/%m")
+                self.publish(db, next_month, entry)
+            return (too_old, " -> ".join(list(db.get_last_reminder())))
+
+    def get_next_month(self, month):
+        return (month + timedelta(days=32)).replace(day=1).strftime("%Y/%m")
 
     def publish(self, db, date, text):
         db.insert_reminder(date, text)
